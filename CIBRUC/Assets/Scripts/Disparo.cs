ae3a96f0ff1fc0e2360a903ce146455c1d64e762
LorenzoJacome <lorenzo.jacome@hotmail.com>
2020-08-07 17:34:16 -0500

    Fixed bug with canon

--- CIBRUC/Assets/Scripts/Disparo.cs
+++ CIBRUC/Assets/Scripts/Disparo.cs
@@ -40,12 +40,12 @@ public class Disparo : MonoBehaviour
     void Update()
     {
         Touch touch = Input.GetTouch(0);
-        direccionBala = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
-        angulo = Mathf.Atan2(direccionBala.y, direccionBala.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angulo - 90f + 70f);
-
-
-
+        if (touch.position.x > Screen.width / 2) {
+            direccionBala = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
+            angulo = Mathf.Atan2(direccionBala.y, direccionBala.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, angulo - 90f + 70f);
+        }
+        
 
     }
 
