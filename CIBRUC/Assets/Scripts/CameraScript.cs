5dcbe0c66ab13c13ace52ff75bf8149d8cc0cea0
LorenzoJacome <lorenzo.jacome@hotmail.com>
2020-08-07 18:02:58 -0500

    Added default background

---
+++ CIBRUC/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Transform transform;
+    public float shakeDuration;
+    public float shakeMagnitude;
+    public float dampingSpeed;
+    Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+
+    void Awake() {
+        if (transform == null) {
+            transform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable() {
+        initialPosition = transform.localPosition;
+    }
+}
---
+++ CIBRUC/Assets/Scripts/mainmenu2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mainmenu2 : MonoBehaviour {
+
+
+
+    [SerializeField] string cambiar="Nivel 1";
+    public void Nivel()
+    {
+        SceneManager.LoadScene(cambiar,LoadSceneMode.Single);
+    }
+   
+
+
+}
