c53dbf1bcb17c4fe06933035b245d198fc749111
DIEGOVELAZ24 <42215166+DIEGOTEC18@users.noreply.github.com>
2020-08-11 23:37:01 -0500

    Create tutorial screen with animations and scripts to controll that animations via the inspector. Correct font on disparar button.

---
+++ CIBRUC/Assets/PanelControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelControl : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void showPanel(bool show) {
+
+            panel.SetActive(show);
+
+    }
+
+}
